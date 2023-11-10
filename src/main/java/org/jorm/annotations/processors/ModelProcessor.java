77b43a4d9a8809d84a866743bf338dcbbdfb89f4
HossameTor <hossametorchi07@gmail.com>
2023-11-10 07:44:00 +0100

    Added Model processor

--- src/main/java/org/jorm/annotations/processors/ModelProcessor.java
+++ src/main/java/org/jorm/annotations/processors/ModelProcessor.java
@@ -1,27 +1,44 @@
 package org.jorm.annotations.processors;
 
-import org.jorm.annotations.Model;
+import com.google.auto.service.AutoService;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
+
+import org.jorm.DatabaseConnection;
+import org.jorm.annotations.Model;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Set;
+
 @SupportedAnnotationTypes("org.jorm.annotations.Model")
 @SupportedSourceVersion(SourceVersion.RELEASE_19)
+@AutoService(Processor.class)
 public class ModelProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        for (Element element : roundEnv.getElementsAnnotatedWith(Model.class)){
-            if (element.getKind().isClass()) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
-                        "Annotated class: " + element.getSimpleName());
+        for (Element element : roundEnv.getElementsAnnotatedWith(Model.class)) {
+            if (element instanceof TypeElement) {
+                TypeElement typeElement = (TypeElement) element;
+                createTable(typeElement.getSimpleName().toString());
             }
         }
         return true;
+    }
+
+    private void createTable(String tableName) {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        Connection con = db.getConn();
+        Statement statement = null;
+        try {
+            statement = con.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS " + tableName + " (id INT AUTO_INCREMENT,PRIMARY KEY (id));");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
