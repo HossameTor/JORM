5ee10247bb31f31de99734aa53d399658c86c601
HossameTor <hossametorchi07@gmail.com>
2023-11-13 03:12:31 +0100

    Updated constraint annotation to use enum for constraint types

---
+++ src/main/java/org/jorm/ConstraintType.java
@@ -0,0 +1,6 @@
+package org.jorm;
+
+public enum ConstraintType {
+    NotNull,
+    UNIQUE
+}
--- src/main/java/org/jorm/annotations/processors/QonstraintProcessor.java
+++ src/main/java/org/jorm/annotations/processors/QonstraintProcessor.java
@@ -1,20 +1,30 @@
 package org.jorm.annotations.processors;
 
 import com.google.auto.service.AutoService;
+import org.jorm.ConstraintType;
+import org.jorm.DatabaseConnection;
 import org.jorm.annotations.Constraint;
+import org.jorm.annotations.Model;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 @SupportedAnnotationTypes("org.jorm.annotations.Constraint")
 @SupportedSourceVersion(SourceVersion.RELEASE_19)
 @AutoService(Processor.class)
 public class ConstraintProcessor extends AbstractProcessor {
-    private String fieldStatement = "(";
 
-    public boolean process(List<Element> elements) {
+
+    /*public boolean process(List<Element> elements) {
         boolean firstElement = true;
         for (Element element : elements) {
             if (element.getAnnotation(Constraint.class)!= null){
@@ -39,8 +49,8 @@ public class ConstraintProcessor extends AbstractProcessor {
             }
         }
         return true;
-    }
-    private String FieldTypeMapper(VariableElement f){
+    }*/
+    private String fieldTypeMapper(VariableElement f){
         switch (f.asType().toString()){
             case "int":
                 return "INT";
@@ -62,12 +72,38 @@ public class ConstraintProcessor extends AbstractProcessor {
                 return f.asType().toString().toUpperCase();
         }
     }
-    public String getFieldsStatement(){
-        return fieldStatement;
-    }
-
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return false;
+        for (Element element : roundEnv.getElementsAnnotatedWith(Constraint.class)) {
+            if (element instanceof VariableElement){
+                TypeElement superC =(TypeElement) element.getEnclosingElement();
+                String query = "ALTER TABLE " + superC.getSimpleName().toString()+"\n";
+                Constraint constraint = element.getAnnotation(Constraint.class);
+                List<ConstraintType> types = Arrays.asList(constraint.types());
+                if (types.contains(ConstraintType.NotNull)){
+                    query+="MODIFY COLUMN "+element.getSimpleName().toString()+" "+ fieldTypeMapper((VariableElement) element)+" NOT NULL";
+                    if (types.contains(ConstraintType.UNIQUE)) query+= ",";
+                }
+                if (types.contains(ConstraintType.UNIQUE)){
+                    query+="ADD CONSTRAINT unique_"+element.getSimpleName().toString()+"_constraint UNIQUE ("+
+                            element.getSimpleName().toString()+");";
+                }
+                System.out.println(query);
+                alterTable(query);
+            }
+        }
+
+        return true;
+    }
+    private void alterTable(String query) {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        Connection con = db.getConn();
+        Statement statement = null;
+        try {
+            statement = con.createStatement();
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
